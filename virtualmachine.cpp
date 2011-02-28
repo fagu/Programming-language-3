8cb5d36a5f5d3ff9147f19f3e485749d5d275180
Fabian Gundlach <320pointsguy@googlemail.com>
2011-02-28 19:21:12 +0100

    VM Instructions put into INSTRUCTION macro

--- virtualmachine.cpp
+++ virtualmachine.cpp
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include "virtualmachine.h"
 #include "garbagecollector.h"
 
@@ -15,8 +16,16 @@ vector<vector<int> > liste;
 vector<vector<int> > stops;
 vector<vector<int> > argsizes;
 
-char opcodes[] = {'A','I','+','-','*','/','%','C','P','R','S','G','J','j','c','=','<','>','(',')','~','&','O'};
-int oplength[] = { 1,  2,  3,  3,  3,  3,  3,  3,  2,  2,  3 , 3 , 2,  1 , 1 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 };
+#define INSTRUCTION(c,n,code) c,
+char opcodes[] = {
+#include "vminstructions.cpp"
+};
+#undef INSTRUCTION
+#define INSTRUCTION(c,n,code) n,
+int oplength[] = {
+#include "vminstructions.cpp"
+};
+#undef INSTRUCTION
 
 state stat;
 
@@ -77,21 +86,9 @@ int main(int argc, char *argv[]) {
 			for (int i = 0; i < sizeof(opcodes)/sizeof(char); i++)
 				if (opcodes[i] == op)
 					liste.back()[N] = i;
-			if (liste.back()[N] == -1 && op != 'M')
+			if (liste.back()[N] == -1)
 				fprintf(stderr, "Unknown command '%c'!\n", op);
-			if (op == 'A' || op == 'j') {
-				liste.back().push_back(0);
-				sscanf(codepos+1, "%d", &liste.back()[N+1]);
-				N += 2;
-			} else if (op == 'I' || op == 'P' || op == 'R' || op == 'J') {
-				liste.back().push_back(0); liste.back().push_back(0);
-				sscanf(codepos+1, "%d;%d", &liste.back()[N+1], &liste.back()[N+2]);
-				N += 3;
-			} else if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%' || op == '=' || op == '<' || op == '>' || op == '(' || op == ')' || op == '~' || op == '&' || op == 'O' || op == 'C' || op == 'S' || op == 'G') {
-				liste.back().push_back(0); liste.back().push_back(0); liste.back().push_back(0);
-				sscanf(codepos+1, "%d;%d;%d", &liste.back()[N+1], &liste.back()[N+2], &liste.back()[N+3]);
-				N += 4;
-			} else if (op == 'c') {
+			if (op == 'c') {
 				int func;
 				char *pos;
 				for (pos = codepos; *pos != '\0'; pos++)
@@ -111,6 +108,18 @@ int main(int argc, char *argv[]) {
 				for (pos--; pos != codepos+1; pos--)
 					if (*pos == '\0')
 						*pos = ';';
+			} else if (oplength[liste.back()[N]] == 1) {
+				liste.back().push_back(0);
+				sscanf(codepos+1, "%d", &liste.back()[N+1]);
+				N += 2;
+			} else if (oplength[liste.back()[N]] == 2) {
+				liste.back().push_back(0); liste.back().push_back(0);
+				sscanf(codepos+1, "%d;%d", &liste.back()[N+1], &liste.back()[N+2]);
+				N += 3;
+			} else if (oplength[liste.back()[N]] == 3) {
+				liste.back().push_back(0); liste.back().push_back(0); liste.back().push_back(0);
+				sscanf(codepos+1, "%d;%d;%d", &liste.back()[N+1], &liste.back()[N+2], &liste.back()[N+3]);
+				N += 4;
 			} else {
 				fprintf(stderr, "Unknown command '%c'!\n", op);
 			}
@@ -143,169 +152,9 @@ int main(int argc, char *argv[]) {
 			int nextpos;
 			aktpos++;
 			switch(op) {
-				case 0:
-					len = li[aktpos];
-					if (len > 0) {
-						for (int i = 0; i < len; i++) {
-							st.push_back(0);
-							ip.push_back(false);
-						}
-					} else {
-						for (int i = 0; i < -len; i++) {
-							st.pop_back();
-							ip.push_back(false);
-						}
-					}
-					nextpos = aktpos+1;
-					break;
-				case 1:
-					co = li[aktpos]; pos = li[aktpos+1];
-					st[li[aktpos+1]] = li[aktpos];
-					ip[li[aktpos+1]] = false;
-					nextpos = aktpos+2;
-					break;
-				case 2:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa]+st[posb];
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 3:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa]-st[posb];
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 4:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa]*st[posb];
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 5:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa]/st[posb];
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 6:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa]%st[posb];
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 7:
-					posa = li[aktpos]; len = li[aktpos+1]; posb = li[aktpos+2];
-					for (int i = 0; i < len; i++) {
-						st[posb+i] = st[posa+i];
-						ip[posb+i] = ip[posa+i];
-					}
-					nextpos = aktpos+3;
-					break;
-				case 8:
-					pos = li[aktpos]; len = li[aktpos+1];
-					for (int i = 0; i < len; i++)
-						printf("%d ", st[pos+i]);
-					printf("\n");
-					nextpos = aktpos+2;
-					break;
-				case 9:
-					pos = li[aktpos]; len = li[aktpos+1];
-					st[pos] = alloc(len, stat);
-					ip[pos] = true;
-					nextpos = aktpos+2;
-					break;
-				case 10:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					stat.hash[st[posb]+posc] = st[posa];
-					stat.hashispointer[st[posb]+posc] = ip[posa];
-					nextpos = aktpos+3;
-					break;
-				case 11:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = stat.hash[st[posa]+posb];
-					ip[posc] = stat.hashispointer[st[posa]+posb];
-					nextpos = aktpos+3;
-					break;
-				case 12:
-					posa = li[aktpos]; posb = li[aktpos+1];
-					if (st[posa] == 0)
-						nextpos = sto[posb];
-					else
-						nextpos = aktpos+2;
-					break;
-				case 13:
-					posa = li[aktpos];
-					nextpos = sto[posa];
-					break;
-				case 14:
-					co = li[aktpos];
-					stat.stac.push_back(new stackentry());
-					stat.stac.back()->aktpos = 0;
-					stat.stac.back()->funcnum = co;
-					se.aktpos = aktpos;
-					for (int i = 0; i < argsizes[co].size(); i++) {
-						for (int k = 0; k < argsizes[co][i]; k++) {
-							stat.stac.back()->regs.push_back(st[li[aktpos+i+1]+k]);
-							stat.stac.back()->ispointer.push_back(ip[li[aktpos+i+1]+k]);
-						}
-						se.aktpos++;
-					}
-					/*printf("called: (%d): ", stat.stac.back()->regs.size());
-					for (int r = 0; r < stat.stac.back()->regs.size(); r++)
-						printf("%d ", stat.stac.back()->regs[r]);
-					printf("\n");*/
-					se.aktpos++;
-					goto stackup;
-					break;
-				case 15:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] == st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 16:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] < st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 17:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] > st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 18:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] <= st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 19:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] >= st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 20:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = st[posa] != st[posb] ? 1 : 0;
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 21:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = (st[posa]!=0 && st[posb]!=0 ? 1 : 0);
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
-				case 22:
-					posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
-					st[posc] = (st[posa]!=0 || st[posb]!=0 ? 1 : 0);
-					ip[posc] = false;
-					nextpos = aktpos+3;
-					break;
+#define INSTRUCTION(c,n,code) case __COUNTER__: code break;
+#include "vminstructions.cpp"
+#undef INSTRUCTION
 				default:
 					fprintf(stderr, "Unknown command %d!\n", op);
 					return 0;
---
+++ vminstructions.cpp
@@ -0,0 +1,186 @@
+
+INSTRUCTION('A',1,
+	len = li[aktpos];
+	if (len > 0) {
+		for (int i = 0; i < len; i++) {
+			st.push_back(0);
+			ip.push_back(false);
+		}
+	} else {
+		for (int i = 0; i < -len; i++) {
+			st.pop_back();
+			ip.push_back(false);
+		}
+	}
+	nextpos = aktpos+1;
+)
+
+INSTRUCTION('I',2,
+	co = li[aktpos]; pos = li[aktpos+1];
+	st[li[aktpos+1]] = li[aktpos];
+	ip[li[aktpos+1]] = false;
+	nextpos = aktpos+2;
+)
+
+INSTRUCTION('C',3,
+	posa = li[aktpos]; len = li[aktpos+1]; posb = li[aktpos+2];
+	for (int i = 0; i < len; i++) {
+		st[posb+i] = st[posa+i];
+		ip[posb+i] = ip[posa+i];
+	}
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('P',2,
+	pos = li[aktpos]; len = li[aktpos+1];
+	for (int i = 0; i < len; i++)
+		printf("%d ", st[pos+i]);
+	printf("\n");
+	nextpos = aktpos+2;
+)
+
+INSTRUCTION('R',2,
+	pos = li[aktpos]; len = li[aktpos+1];
+	st[pos] = alloc(len, stat);
+	ip[pos] = true;
+	nextpos = aktpos+2;
+)
+
+INSTRUCTION('S',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	stat.hash[st[posb]+posc] = st[posa];
+	stat.hashispointer[st[posb]+posc] = ip[posa];
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('G',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = stat.hash[st[posa]+posb];
+	ip[posc] = stat.hashispointer[st[posa]+posb];
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('J',2,
+	posa = li[aktpos]; posb = li[aktpos+1];
+	if (st[posa] == 0)
+		nextpos = sto[posb];
+	else
+		nextpos = aktpos+2;
+)
+
+INSTRUCTION('j',1,
+	posa = li[aktpos];
+	nextpos = sto[posa];
+)
+
+INSTRUCTION('c',1,
+	co = li[aktpos];
+	stat.stac.push_back(new stackentry());
+	stat.stac.back()->aktpos = 0;
+	stat.stac.back()->funcnum = co;
+	se.aktpos = aktpos;
+	for (int i = 0; i < argsizes[co].size(); i++) {
+		for (int k = 0; k < argsizes[co][i]; k++) {
+			stat.stac.back()->regs.push_back(st[li[aktpos+i+1]+k]);
+			stat.stac.back()->ispointer.push_back(ip[li[aktpos+i+1]+k]);
+		}
+		se.aktpos++;
+	}
+	/*printf("called: (%d): ", stat.stac.back()->regs.size());
+	for (int r = 0; r < stat.stac.back()->regs.size(); r++)
+		printf("%d ", stat.stac.back()->regs[r]);
+	printf("\n");*/
+	se.aktpos++;
+	goto stackup;
+)
+
+INSTRUCTION('+',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa]+st[posb];
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('-',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa]-st[posb];
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('*',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa]*st[posb];
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('/',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa]/st[posb];
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('%',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa]%st[posb];
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('=',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] == st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('<',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] < st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('>',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] > st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('(',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] <= st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION(')',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] >= st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('~',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = st[posa] != st[posb] ? 1 : 0;
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('&',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = (st[posa]!=0 && st[posb]!=0 ? 1 : 0);
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
+
+INSTRUCTION('O',3,
+	posa = li[aktpos]; posb = li[aktpos+1]; posc = li[aktpos+2];
+	st[posc] = (st[posa]!=0 || st[posb]!=0 ? 1 : 0);
+	ip[posc] = false;
+	nextpos = aktpos+3;
+)
